68b7d7fc949cc3cfbf360497f4f38e0067fdb2a2
Pradeep H R <150323297+pradeep-hr@users.noreply.github.com>
2025-02-18 12:59:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let celciusInput = document.querySelector('#celcius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
+
+let btn = document.querySelector('.button button')
+
+function roundNumber(number) {
+    return Math.round(number*100)/100
+}
+
+celciusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celciusInput.value)
+    let fTemp = (cTemp*(9/5)) + 32
+    let kTemp = cTemp + 273.15
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value)
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = (fTemp - 32) * (5/9) + 273.15
+
+    celciusInput.value = roundNumber(cTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+kelvinInput.addEventListener('input', function(){
+    let kTemp = parseFloat(kelvinInput.value)
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
+
+    celciusInput.value = roundNumber(cTemp)
+    fahrenheitInput.value = roundNumber(fTemp)
+})
+
+btn.addEventListener('click', ()=> {
+    celciusInput.value = ""
+    fahrenheitInput.value = ""
+    kelvinInput.value = ""
+})
+
+function updateResponsiveSize() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    
+    if (width < 768) { // Mobile devices
+        document.body.style.fontSize = "14px";
+    } else if (width < 1024) { // Tablets
+        document.body.style.fontSize = "16px";
+    } else { // Desktops
+        document.body.style.fontSize = "18px";
+    }
+}
+
+// Initial call
+updateResponsiveSize();
+
+// Update on resize
+window.addEventListener("resize", updateResponsiveSize);
